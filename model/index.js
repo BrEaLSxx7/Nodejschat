63d8b0ce6a1ea5b44c5faa6d60264340ab412805
Sebastian cano cadavid <sebascano65@gmail.com>
2017-10-21 22:08:07 -0500

    Update index.js

--- model/index.js
+++ model/index.js
@@ -1,10 +1,10 @@
 var { Pool } = require('pg');
 
 var pool = new Pool({
-	user: 'postgres',
-	host: 'localhost',
-	database: 'mydb',
-	password: 'root',
+	user: 'ynpdmtrjmirjwe',
+	host: 'ec2-54-163-235-175.compute-1.amazonaws.com',
+	database: 'd701ckcj28dlju',
+	password: '7549cfa56023cd745bf5810368588c1de4c9b03cbe25a5e6fdee451bdf978d59',
 	port: 5432
 });
 
