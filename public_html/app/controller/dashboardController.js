1190d9a20fcbabb8810122def12b9dc4dafd93ef
BrEaLSxx7 <sebascano65@gmail.com>
2017-10-22 23:44:30 -0500

    aplicando socket

--- public_html/app/controller/dashboardController.js
+++ public_html/app/controller/dashboardController.js
@@ -5,6 +5,7 @@
 		.controller('dashboardController', dashboardController);
 	dashboardController.$inject = ['$scope', 'DataBaseService', '$log', '$mdToast', '$state'];
 	function dashboardController($scope, DataBase, $log, $mdToast, $state) {
+		var socket = io.connect('https://brealsxx.herokuapp.com/', { 'forcenew': true });
 		$scope.currentNavItem = 'Chat';
 		$scope.logout = () => {
 			sessionStorage.clear();
